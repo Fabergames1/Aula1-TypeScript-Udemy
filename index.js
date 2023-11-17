32f65c5af775322ce9a49356bd3bbc9c46d36fc3
Fabergames1 <faber.games@hotmail.com>
2023-11-17 02:59:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function somar(a, b) {
+    return a + b;
+}
+var a = 50;
+var b = 50;
+var soma = somar(a, b);
+console.log(soma);
---
+++ index.ts
@@ -0,0 +1,7 @@
+function somar(a: number, b: number): number {
+  return a + b;
+}
+const a = 50;
+const b = 50;
+const soma = somar(a, b);
+console.log(soma); 
